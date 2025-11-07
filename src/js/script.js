8b3ec3e6713d1dce9b837e14632757b4c5a6b37f
jehat12 <jehat.bakiray@edu.univ-fcomte.fr>
2025-11-07 09:57:49 +0100

    ajout script

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,16 @@
-aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
 aMettreEnRouge.style.color = "red"
+
+const bouton = document.querySelector("#en-rouge-suite-a-click");
+
+bouton.addEventListener("click", () => {
+  bouton.style.color = "red"; // Le texte devient rouge au clic
+});
+
+const titres = document.querySelectorAll("h2");
+
+titres.forEach((titre) => {
+  titre.addEventListener("click", (event) => {
+    event.target.style.color = "red"; // l'élément cliqué devient rouge
+  });
+});
